c02c192a396a486235f388fd9aeeb641991750cd
alexa <alexa@DESKTOP-SF3JQH1>
2024-04-22 07:34:42 -0500

    Add project files.

---
+++ Evaluacion1PMatiasRobayo/Models/Carrera.cs
@@ -0,0 +1,11 @@
+﻿namespace Evaluacion1PMatiasRobayo.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string nombre_carrera { get; set; }
+        public string campus { get; set; }
+        public int numero_semestres { get; set; }
+
+    }
+}
---
+++ Evaluacion1PMatiasRobayo/Models/MRobayo.cs
@@ -0,0 +1,12 @@
+﻿namespace Evaluacion1PMatiasRobayo.Models
+{
+    public class MRobayo
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public float Promedio { get; set;}
+        public bool Estudia { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
